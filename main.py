637ab1cf00d91b6433ce0a7175645980c1305642
saintabomination <saint.abomination@protonmail.com>
2022-09-14 10:22:38 +0200

    Reading window settings from a file

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pyglet
+import constants
 
-window = pyglet.window.Window()
+window = pyglet.window.Window(
+  constants.WINDOW_WIDTH,
+  constants.WINDOW_HEIGHT,
+  fullscreen=constants.WINDOW_FULLSCREEN,
+  caption=constants.WINDOW_TITLE
+)
 
 @window.event
 def on_draw():
